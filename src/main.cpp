bd1bae2a242831213c3f68892e2cb14ae1a1faf0
Yay899 <yay899@gmail.com>
2017-10-19 21:03:23 -0400

    Lacking a whiteboard, I've shoved a bunch of comments in here.

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+
+int main(int argc, char *argv[]) {
+
+	/*
+		Init stuff goes here.
+	*/
+
+	//Main game loop.
+	while (true) { //TODO If we're using LibTCOD, terminate the loop when the window is closed.
+
+		/*
+			Maybe break this this up into two functions or so.
+				One for game calculations, like state.update(); or whatever.
+				And then the other for rendering state.render(); or something.
+
+			Basically to keep us from constantly needing to change main();
+		*/
+
+	}
+
+	return 0;
+}
