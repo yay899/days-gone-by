9dcaae9e8f89a54282d672bf595f1961c3f1ea44
JMT97 <jacktoth97@gmail.com>
2017-12-23 16:24:14 -0500

    Forgot Entity

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -7,20 +7,36 @@ extern Engine _eng;
 /*
 	Class Entity.
 */
-Entity::Entity(int x, int y, char c, TCODColor col) : x(x), y(y), c(c), col(col), hp(0), maxHp(0), watts(0), maxWatts(0) {
-
+Entity::Entity(int x, int y, char c, bool player, TCODColor col) : x(x), y(y), c(c), col(col){
+	if(player){
+		maxHp = DEFAULT_PLAYER_MAXHP;
+		maxWatts = DEFAULT_PLAYER_MAXWATTS;
+		ai = new PlayerAI();
+	}
+	else{
+		maxHp = DEFAULT_TESTENEMY_MAXHP;
+		maxWatts = DEFAULT_TESTENEMY_MAXWATTS;
+		ai = new EnemyAI();
+	}
+	hp = maxHp;
+	watts = maxWatts;
+}
+Entity::~Entity(){
+	delete ai;
 }
 
-void Entity::update(float t, TCOD_key_t key, Map* map) {
-	//Define this when extending.
+void Entity::update(TCOD_key_t key, Map* map) {
+	ai->takeTurn(map);
 }
 
 void Entity::render() {
-	//Define this when extending.
+	TCODConsole::root->setCharForeground(x, y, col);
+	TCODConsole::root->setChar(x, y, c);
 }
 
 void Entity::attack(unsigned int targetX, unsigned int targetY, Map* map) {
-	//Define this when extending.
+	Entity* target = map->getEntity(targetX, targetY);
+	if (target != nullptr) target->hurt(4);
 }
 
 void Entity::move(unsigned int targetX, unsigned int targetY, Map* map) {
@@ -75,132 +91,4 @@ bool Entity::drain(int x) {
 
 void Entity::kill() {
 	col = TCOD_red; //Test code. Just makes the entity red.
-}
-
-/*
-	Class EntityPlayer.
-*/
-EntityPlayer::EntityPlayer(int x, int y, char c, TCODColor col) : Entity(x, y, c, col) {
-
-}
-
-void EntityPlayer::update(float t, TCOD_key_t key, Map* map) {
-
-	switch (key.vk) {
-	//Characters.
-	case TCODK_CHAR:
-		switch (key.c) {
-		case '.':
-			_eng.gameState = STATE_AI_TURN;
-		default:
-			break;
-		}
-		break;
-	//Special keys.
-	case TCODK_UP:
-		move(x, y - 1, map);
-		_eng.gameState = STATE_AI_TURN;
-		break;
-	case TCODK_DOWN:
-		move(x, y + 1, map);
-		_eng.gameState = STATE_AI_TURN;
-		break;
-	case TCODK_LEFT:
-		move(x - 1, y, map);
-		_eng.gameState = STATE_AI_TURN;
-		break;
-	case TCODK_RIGHT:
-		move(x + 1, y, map);
-		_eng.gameState = STATE_AI_TURN;
-		break;
-	case TCODK_KP1:
-		move(x - 1, y + 1, map);
-		_eng.gameState = STATE_AI_TURN;
-		break;
-	case TCODK_KP2:
-		move(x, y + 1, map);
-		_eng.gameState = STATE_AI_TURN;
-		break;
-	case TCODK_KP3:
-		move(x + 1, y + 1, map);
-		_eng.gameState = STATE_AI_TURN;
-		break;
-	case TCODK_KP4:
-		move(x - 1, y, map);
-		_eng.gameState = STATE_AI_TURN;
-		break;
-	case TCODK_KP5:
-		_eng.gameState = STATE_AI_TURN;
-		break;
-	case TCODK_KP6:
-		move(x + 1, y, map);
-		_eng.gameState = STATE_AI_TURN;
-		break;
-	case TCODK_KP7:
-		move(x - 1, y - 1, map);
-		_eng.gameState = STATE_AI_TURN;
-		break;
-	case TCODK_KP8:
-		move(x, y - 1, map);
-		_eng.gameState = STATE_AI_TURN;
-		break;
-	case TCODK_KP9:
-		move(x + 1, y - 1, map);
-		_eng.gameState = STATE_AI_TURN;
-		break;
-	default:
-		break;
-	}
-}
-
-void EntityPlayer::render() {
-	TCODConsole::root->setCharForeground(x, y, col);
-	TCODConsole::root->setChar(x, y, c);
-}
-
-void EntityPlayer::attack(unsigned int x, unsigned int y, Map* map) {
-	Entity* target = map->getEntity(x, y);
-	if (target != nullptr) target->hurt(4);
-}
-
-/*
-	Class EntityTestEnemy
-*/
-
-EntityTestEnemy::EntityTestEnemy(int x, int y, char c, TCODColor col) : Entity(x, y, c, col) {
-
-}
-
-void EntityTestEnemy::update(float t, TCOD_key_t key, Map* map) {
-	int targetX(x), targetY(y);
-
-	//Target one tile in the direction of the player.
-	if (map->teamPlayer.at(0)->x > x) {
-		targetX++;
-	}
-	else if (map->teamPlayer.at(0)->x < x) {
-		targetX--;
-	}
-
-	if (map->teamPlayer.at(0)->y > y) {
-		targetY++;
-	}
-	else if (map->teamPlayer.at(0)->y < y) {
-		targetY--;
-	}
-
-	//Move if the target is actually somewhere other than current position.
-	if (targetX != x || targetY != y) {
-		move(targetX, targetY, map);
-	}
-}
-
-void EntityTestEnemy::render() {
-	TCODConsole::root->setCharForeground(x, y, col);
-	TCODConsole::root->setChar(x, y, c);
-}
-
-void EntityTestEnemy::attack(unsigned int x, unsigned int y, Map* map) {
-	Entity* target = map->getEntity(x, y);
-	if (target != nullptr) target->hurt(1);
 }
